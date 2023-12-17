df135acfc7f12e070dee657ae66e962922a19793
Sandra <sandrasuazo96@gmail.com>
2023-12-17 23:30:04 +0100

    Feat: Added the nutrition model

---
+++ src/models/nutrition-model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const nutritionSchema = new Schema(
+  {
+    type: {
+      type: String,
+      enum: ["balanceDiet", "proteinDiet", "ketogenicDiet", "paleoDiet"],
+      required: true,
+    },
+    description: { type: String, required: true },
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+export const Nutrition = mongoose.model("Nutrition", nutritionSchema);
