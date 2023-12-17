9fe8d62e7e6120bb5a462e4a8a687f7f3a43d4af
Sandra <sandrasuazo96@gmail.com>
2023-12-17 20:17:09 +0100

    Feat: Added  the user model

---
+++ src/models/user-model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 200 },
+    phone: { type: Number, unique: true, minlength: 9 },
+    adress: { type: String, minlength: 5 },
+    email: { type: String, required: true, unique: true, maxlength: 50 },
+    password: { type: String, required: true, select: false, maxlenght: 15 },
+    role: {
+      type: String,
+      enum: ["admin", "customer", "monitor"],
+      required: true,
+    },
+    isActive: { type: Boolean, required: true },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
