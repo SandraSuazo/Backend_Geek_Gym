ac5da1df6259a066e3eb32664ca97870fb9271d3
Sandra <sandrasuazo96@gmail.com>
2023-12-23 21:21:13 +0100

    fix: add parseInt in HASH_ROUNDS of config.js

--- src/core/config.js
+++ src/core/config.js
@@ -2,5 +2,5 @@ export const CONFIG = {
   DB_URL: process.env.DB_URL,
   PORT: process.env.PORT,
   SECRET: process.env.SECRET,
-  HASH_ROUNDS: process.env.HASH_ROUNDS,
+  HASH_ROUNDS: parseInt(process.env.HASH_ROUNDS),
 };
