82705a7f8b0bbf03e652721fedbc9aa76bb624e8
Sandra <sandrasuazo96@gmail.com>
2023-12-23 21:15:26 +0100

    fix: add userRouter in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { CONFIG } from "./core/config.js";
+import userRouter from "./routes/user-route.js";
 import { connectDB } from "./core/db.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/user", userRouter);
 
 app.listen(CONFIG.PORT, () =>
   console.log(`Server listening on port ${CONFIG.PORT}`)
