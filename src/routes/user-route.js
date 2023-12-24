5d96e9f8b6a059ca746320f478d380bdf087e789
Sandra <sandrasuazo96@gmail.com>
2023-12-24 01:49:02 +0100

    feat: create user route

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,15 +1,47 @@
 import express from "express";
-import { registerUser } from "../controllers/user-controller.js";
+import { auth } from "../middlewares/auth.js";
+import { isActive } from "../middlewares/isActive.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+import {
+  profileUser,
+  updateProfile,
+  deactivateUser,
+} from "../controllers/user-controller.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-  const newUser = req.body;
+router.get("/profile", auth, isActive, async (req, res, next) => {
+  const userId = req.user.userId;
   try {
-    res.json(await registerUser(newUser, next));
+    res.json(await profileUser(userId, next));
   } catch (error) {
     next("INTERNAL_SERVER_ERROR");
   }
 });
+
+router.patch("/update-profile", auth, isActive, async (req, res, next) => {
+  const userId = req.user.userId;
+  const updatedData = req.body;
+  try {
+    res.json(await updateProfile(userId, updatedData, next));
+  } catch (error) {
+    next("INTERNAL_SERVER_ERROR");
+  }
+});
+
+router.delete(
+  "/deactivate/:userId",
+  auth,
+  isActive,
+  isAdmin,
+  async (req, res, next) => {
+    const userId = req.params.userId;
+    try {
+      res.json(await deactivateUser(userId, next));
+    } catch (error) {
+      next("INTERNAL_SERVER_ERROR");
+    }
+  }
+);
 
 export default router;
