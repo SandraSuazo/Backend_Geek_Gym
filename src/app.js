b4ee820eaf39ffa0478958c272e17a8686f5369b
Sandra <sandrasuazo96@gmail.com>
2023-12-24 01:47:42 +0100

    fix: add userRoute in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import { CONFIG } from "./core/config.js";
 import authRouter from "./routes/auth-route.js";
-// import userRouter from "./routes/user-route.js";
+import userRouter from "./routes/user-route.js";
+// import activityRouter from "./ruotes/activity-route.js"
 
 import { connectDB } from "./core/db.js";
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", authRouter);
-// app.use("/user", userRouter);
+app.use("/user", userRouter);
 // app.use("/activity", activityRouter);
 
 app.listen(CONFIG.PORT, () =>
