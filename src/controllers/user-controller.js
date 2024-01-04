94c4add767b905d09a7df0826af35934b93c4ea4
Sandra <sandrasuazo96@gmail.com>
2024-01-04 13:56:49 +0100

    feat: add getAllUsers and getUserDetails

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,8 +1,6 @@
-import bcrypt from "bcrypt";
 import { Auth } from "../models/auth-model.js";
 import { User } from "../models/user-model.js";
-import { CONFIG } from "../core/config.js";
-import { validateEmail, validatePassword } from "../shared/validators.js";
+import { validateEmail } from "../shared/validators.js";
 
 export const profileUser = async (userId, next) => {
   const authUser = await Auth.findById(userId);
@@ -17,14 +15,6 @@ export const updateProfile = async (userId, updatedData, next) => {
   if (updatedData.email !== "") {
     validateEmail(updatedData.email, next);
   }
-
-  // if (updatedData.password !== "") {
-  //   validatePassword(updatedData.password, next);
-  //   updatedData.password = await bcrypt.hash(
-  //     updatedData.password,
-  //     CONFIG.HASH_ROUNDS
-  //   );
-  // }
 
   const authUser = await Auth.findByIdAndUpdate(userId, updatedData);
   if (!authUser) {
@@ -41,10 +31,42 @@ export const updateProfile = async (userId, updatedData, next) => {
   return { authUser, user };
 };
 
+export const getAllUsers = async (next) => {
+  const authUsers = await Auth.find();
+  const users = await User.find();
+  if (!authUsers && !users) {
+    throw new Error(next("USER_NOT_FOUND"));
+  }
+  return { authUsers, users };
+};
+
+export const getUserDetails = async (userId, next) => {
+  const authUser = await Auth.findById(userId);
+  const user = await User.findById(userId);
+  if (!authUser && !user) {
+    throw new Error(next("USER_NOT_FOUND"));
+  }
+  return { authUser, user };
+};
+
 export const deactivateUser = async (userId, next) => {
   const authUser = await Auth.findByIdAndUpdate(userId, { isActive: false });
   if (!authUser) {
     throw new Error(next("USER_NOT_FOUND"));
+  }
+  if (authUser.role === "admin") {
+    throw new Error(next("ACCESS_DENIED"));
+  }
+  return authUser;
+};
+
+export const activateUser = async (userId, next) => {
+  const authUser = await Auth.findByIdAndUpdate(userId, { isActive: true });
+  if (!authUser) {
+    throw new Error(next("USER_NOT_FOUND"));
+  }
+  if (authUser.role !== "customer") {
+    throw new Error(next("ACCESS_DENIED"));
   }
   return authUser;
 };
--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -5,25 +5,40 @@ import { isAdmin } from "../middlewares/isAdmin.js";
 import {
   profileUser,
   updateProfile,
+  getAllUsers,
   deactivateUser,
+  activateUser,
 } from "../controllers/user-controller.js";
 
 const router = express.Router();
 
 router.get("/profile", auth, isActive, async (req, res, next) => {
-  const userId = req.user.userId;
   try {
-    res.json(await profileUser(userId, next));
+    res.json(await profileUser(req.user.userId, next));
   } catch (error) {
     next("INTERNAL_SERVER_ERROR");
   }
 });
 
 router.patch("/update-profile", auth, isActive, async (req, res, next) => {
-  const userId = req.user.userId;
-  const updatedData = req.body;
   try {
-    res.json(await updateProfile(userId, updatedData, next));
+    res.json(await updateProfile(req.user.userId, req.body, next));
+  } catch (error) {
+    next("INTERNAL_SERVER_ERROR");
+  }
+});
+
+router.get("/all-users", auth, isAdmin, async (req, res, next) => {
+  try {
+    res.json(await getAllUsers(next));
+  } catch (error) {
+    next("INTERNAL_SERVER_ERROR");
+  }
+});
+
+router.get("/details/:userId", auth, isAdmin, async (req, res, next) => {
+  try {
+    res.json(await getUserDetails(req.params.userId, next));
   } catch (error) {
     next("INTERNAL_SERVER_ERROR");
   }
@@ -35,13 +50,20 @@ router.delete(
   isActive,
   isAdmin,
   async (req, res, next) => {
-    const userId = req.params.userId;
     try {
-      res.json(await deactivateUser(userId, next));
+      res.json(await deactivateUser(req.params.userId, next));
     } catch (error) {
       next("INTERNAL_SERVER_ERROR");
     }
   }
 );
+
+router.patch("/activate/:userId", auth, isAdmin, async (req, res, next) => {
+  try {
+    res.json(await activateUser(req.params.userId, next));
+  } catch (error) {
+    next("INTERNAL_SERVER_ERROR");
+  }
+});
 
 export default router;
